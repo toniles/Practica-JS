495546b02109e30a02553ec258c23ccf0f76e734
yes <yes@gmail.com>
2023-10-29 11:19:18 +0100

    a

--- js/carrito.js
+++ js/carrito.js
@@ -1,16 +1,82 @@
-class Carrito{
-	constructor(id){						
-	}
-						
-	anyadeArticulo(articulo){		
-		}			
-				
-	borraArticulo(codigo){		
-	}
-	
-	modificaUnidades(codigo,n){		
-	}	
-			
-	verCarrito(){
-	}			
+class Carrito {
+    constructor(id) {
+        this.id = id;
+        this.articulos = [];
+    }
+
+    anyadeArticulo(articulo) {
+        let encontrado = this.articulos.find(a => a.codigo === articulo.codigo);
+        if (encontrado) {
+            encontrado.unidades++;
+        } else {
+            articulo.unidades = 1;
+            this.articulos.push(articulo);
+        }
+    }
+
+    borraArticulo(codigoArticulo) {
+        this.articulos = this.articulos.filter(a => a.codigo !== codigoArticulo);
+    }
+
+    verCarrito() {
+        // Mostrar el carrito en el control dialog
+        const dialogContent = document.getElementById("dialogContent");
+        dialogContent.innerHTML = "";  // Limpiar el contenido anterior
+
+        if (this.articulos.length === 0) {
+            dialogContent.innerHTML = "<p>No hay artículos en el carrito</p>";
+            return;
+        }
+
+        let tabla = "<table><thead><tr><th>Nombre</th><th>Unidades</th><th>Precio</th><th>Acciones</th></tr></thead><tbody>";
+        this.articulos.forEach(articulo => {
+            tabla += `<tr>
+                        <td>${articulo.nombre}</td>
+                        <td>${articulo.unidades}</td>
+                        <td>${articulo.precio * articulo.unidades}</td>
+                        <td>
+                            <button onclick="modificarUnidades('${articulo.codigo}', 'incrementar')">+</button>
+                            <button onclick="modificarUnidades('${articulo.codigo}', 'decrementar')">-</button>
+                            <button onclick="borrarArticuloCarrito('${articulo.codigo}')">Eliminar</button>
+                        </td>
+                      </tr>`;
+        });
+        tabla += "</tbody></table>";
+
+        dialogContent.innerHTML = tabla;
+        this.actualizarTotal();
+    }
+
+    modificaUnidades(codigoArticulo, operacion) {
+        let articulo = this.articulos.find(a => a.codigo === codigoArticulo);
+        if (articulo) {
+            if (operacion === "incrementar") {
+                articulo.unidades++;
+            } else if (operacion === "decrementar" && articulo.unidades > 0) {
+                articulo.unidades--;
+                if (articulo.unidades === 0) {
+                    this.borraArticulo(codigoArticulo);
+                }
+            }
+            this.verCarrito();  // Actualizar la vista del carrito
+        }
+    }
+
+    actualizarTotal() {
+        const totalSpan = document.getElementById("total");
+        const total = this.articulos.reduce((acum, articulo) => acum + (articulo.precio * articulo.unidades), 0);
+        totalSpan.textContent = total + " €";
+    }
 }
+
+// Funciones globales para ser llamadas desde el HTML
+function modificarUnidades(codigo, operacion) {
+    carrito.modificaUnidades(codigo, operacion);
+}
+
+function borrarArticuloCarrito(codigo) {
+    carrito.borraArticulo(codigo);
+}
+
+// Creación del carrito global
+const carrito = new Carrito(1);
--- js/tienda.js
+++ js/tienda.js
@@ -1,29 +1,62 @@
-	criterios=["Sin ordenar","Ascendente por precio", "Descendente por precio"]
-	
-	function creaListaCriterios(){
-		
-	}
+const criterios = ["Sin ordenar", "Ascendente por precio", "Descendente por precio"];
 
+function creaListaCriterios() {
+    const select = document.querySelector("select");
+    criterios.forEach(criterio => {
+        let option = document.createElement("option");
+        option.value = criterio;
+        option.textContent = criterio;
+        select.appendChild(option);
+    });
 
-	function pintaArticulos(orden){
-		
-	}
-	
-	
-	function ponArticuloEnCarrito(){
-		
-	}
+    // Evento cuando se cambia la opción de ordenación
+    select.addEventListener("change", function() {
+        pintaArticulos(this.value);
+    });
+}
 
+function pintaArticulos(orden) {
+    const contenedor = document.getElementById("contenedor");
+    let articulosOrdenados = [...listaArticulos];  // Crear una copia del array de artículos
 
-	function verCarro(){
-	
-	}
+    if (orden === "Ascendente por precio") {
+        articulosOrdenados.sort((a, b) => a.precio - b.precio);
+    } else if (orden === "Descendente por precio") {
+        articulosOrdenados.sort((a, b) => b.precio - a.precio);
+    }
 
-	function efectuaPedido(){
-	
-	}
+    contenedor.innerHTML = "";  // Limpiar el contenedor
 
-	window.onload=()=>{
+    articulosOrdenados.forEach(articulo => {
+        const card = `
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${articulo.nombre}</h5>
+                        <p class="card-text">${articulo.descripcion}</p>
+                        <p class="card-text">${articulo.precio} €</p>
+                        <button onclick="ponArticuloEnCarrito('${articulo.codigo}')">Comprar</button>
+                    </div>
+                </div>
+            </div>`;
+        contenedor.innerHTML += card;
+    });
+}
 
-	}
+function ponArticuloEnCarrito(codigo) {
+    const articulo = listaArticulos.find(a => a.codigo === codigo);
+    carrito.anyadeArticulo(articulo);
+}
 
+function verCarro() {
+    carrito.verCarrito();
+}
+
+function efectuaPedido() {
+    console.log(JSON.stringify(carrito));
+}
+
+window.onload = () => {
+    creaListaCriterios();
+    pintaArticulos("Sin ordenar");  // Pintar inicialmente sin ordenar
+};
